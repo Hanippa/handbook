0f4068892a1df3c51156db301e4a079771c936f2
Hanippa <dekelmaz09@gmail.com>
2026-06-06 20:23:59 +0300

    Fix Quartz published asset paths and RTL

--- .github/scripts/fix-screenshot-paths.cjs
+++ .github/scripts/fix-screenshot-paths.cjs
@@ -3,6 +3,13 @@ const path = require("path")
 
 const root = "public"
 const replacements = new Map([
+  ['<html lang="en" dir="ltr">', '<html lang="he" dir="rtl">'],
+  ['href="../../index.css"', 'href="/handbook/index.css"'],
+  ['src="../../prescript.js"', 'src="/handbook/prescript.js"'],
+  ['src="../../postscript.js"', 'src="/handbook/postscript.js"'],
+  ['href="../../static/', 'href="/handbook/static/'],
+  ['fetch("../../static/', 'fetch("/handbook/static/'],
+  ['href="../..">Handbook</a>', 'href="/handbook/">Handbook</a>'],
   [
     "../../handbook/guru/assets/images/open-repair-ticket/",
     "https://raw.githubusercontent.com/Hanippa/handbook/gh-pages/guru/assets/images/open-repair-ticket/",
